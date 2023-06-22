ac1c1726686b892456e1c4eab59ebec3c75ae6aa
Minh Nghĩa <nghiaphamln3@gmail.com>
2023-06-22 14:00:38 +0700

    create expressjs project

---
+++ src/models/user.model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+let userModelSchema = new Schema({
+    google: {
+        id: Number,
+        token: String
+    },
+    fullName: {
+        type: String,
+        default: ''
+    },
+    email: {
+        type: String,
+        default: ''
+    },
+    permission: {
+        type: Number,
+        default: 0
+    },
+    avatar: {
+        type: String,
+        default: '/images/no-avatar.png'
+    },
+    createdDate: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+
+module.exports = mongoose.model('users', userModelSchema);
